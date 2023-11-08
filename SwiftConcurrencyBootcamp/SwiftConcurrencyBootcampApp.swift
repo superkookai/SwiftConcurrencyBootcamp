41adee8d764f49e0ae28731b05ddc4d8064a7139
Weerawut Chaiyasomboon <weerawut@live.com>
2023-11-08 16:56:37 +0700

    Download image using 1. @escaping 2. Combine 3. Async/Await

--- SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct SwiftConcurrencyBootcampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            DownloadImageAsync()
         }
     }
 }
